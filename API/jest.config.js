526592e25bc547c163030561acd58921b4b690e2
Melandry Marchand <melandry27@gmail.com>
2025-06-14 12:13:03 +0200

    chore: add jest-junit for test reporting in CI

--- API/jest.config.js
+++ API/jest.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  setupFiles: ["<rootDir>/src/tests/setup-env.ts"],
-  globalSetup: "<rootDir>/src/tests/global-setup.js",
-  testMatch: ["**/tests/**/*.test.ts"],
+	preset: "ts-jest",
+	testEnvironment: "node",
+	setupFiles: ["<rootDir>/src/tests/setup-env.ts"],
+	globalSetup: "<rootDir>/src/tests/global-setup.js",
+	testMatch: ["**/tests/**/*.test.ts"],
+	reporters: [
+		"default",
+		[
+			"jest-junit",
+			{
+				outputDirectory: "test-results",
+				outputName: "jest-junit.xml",
+			},
+		],
+	],
 };
