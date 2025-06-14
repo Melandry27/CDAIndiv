cd3d931429a4b4b702fb28a5acb6e2e16519b4fd
Melandry Marchand <melandry27@gmail.com>
2025-06-14 12:33:43 +0200

    fix: update package.json and package-lock.json to add @types/node and vite-tsconfig-paths dependencies; refactor tsconfig.json and vite.config.ts for improved structure

--- BACKOFFICE/vite.config.ts
+++ BACKOFFICE/vite.config.ts
@@ -1,14 +1,19 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [tailwindcss(), react(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
 });
