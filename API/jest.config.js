e3c936023778b45e83d48c623c267daadfb9a612
Melandry <msmelandry@hotmail.fr>
2025-06-18 13:31:45 +0200

    fix: update lint command to use --legacy-peer-deps for mobile app

--- API/jest.config.js
+++ API/jest.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-	preset: "ts-jest",
-	testEnvironment: "node",
-	setupFiles: ["<rootDir>/src/tests/setup-env.ts"],
-	globalSetup: "<rootDir>/src/tests/global-setup.js",
-	testMatch: ["**/tests/**/*.test.ts"],
-	reporters: [
-		"default",
-		[
-			"jest-junit",
-			{
-				outputDirectory: "test-results",
-				outputName: "jest-junit.xml",
-			},
-		],
-	],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFiles: ["<rootDir>/src/tests/setup-env.ts"],
+  testMatch: ["**/tests/**/*.test.ts"],
+  setupFilesAfterEnv: ["<rootDir>/test-files/setup-env.ts"],
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "test-results",
+        outputName: "jest-junit.xml",
+      },
+    ],
+  ],
 };
